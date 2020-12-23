188bac25dbb93414da1a56be605776c16ae33d43
Gobukgol <zxczc159@gmail.com>
2020-12-23 16:08:17 +0900

    feat(BomPlatformApplication) : BomPlatformApplication 추가

---
+++ bomplatform-web/src/main/java/com/javabom/bomplatform/web/main/BomPlatformApplication.java
@@ -0,0 +1,11 @@
+package com.javabom.bomplatform.web.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BomPlatformApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BomPlatformApplication.class, args);
+    }
+}
