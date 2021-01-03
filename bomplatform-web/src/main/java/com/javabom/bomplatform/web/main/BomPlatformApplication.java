7d0d11d5bf5e5f6a65233b919917a6caac4cd371
Gobukgol <zxczc159@gmail.com>
2021-01-03 16:31:06 +0900

    feat(WebConfig): 다른 모듈의 Configuration 클래스를 Import 하는 WebConfig.java 추가

--- bomplatform-web/src/main/java/com/javabom/bomplatform/web/main/BomPlatformApplication.java
+++ bomplatform-web/src/main/java/com/javabom/bomplatform/web/main/BomPlatformApplication.java
@@ -1,9 +1,12 @@
 package com.javabom.bomplatform.web.main;
 
+import com.javabom.bomplatform.web.config.WebConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(WebConfig.class)
 public class BomPlatformApplication {
     public static void main(String[] args) {
         SpringApplication.run(BomPlatformApplication.class, args);
