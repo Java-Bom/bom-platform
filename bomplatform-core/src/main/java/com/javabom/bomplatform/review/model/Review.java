3742ec6231529fb48d177dd9ef4282a997b83875
BangKiHyun <rlrlvh@naver.com>
2021-01-11 14:53:07 +0900

    feat: add domain, repository

---
+++ bomplatform-core/src/main/java/com/javabom/bomplatform/review/model/Review.java
@@ -0,0 +1,45 @@
+package com.javabom.bomplatform.review.model;
+
+import com.javabom.bomplatform.progressmission.model.ProgressMission;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Review {
+
+    @Id
+    @Column(name = "review_id")
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "progress_mission_id")
+    private ProgressMission progressMission;
+
+    @Column(name = "review_url")
+    private String reviewURL;
+
+    @Enumerated(EnumType.STRING)
+    private ReviewState reviewState;
+
+    public Review(ProgressMission progressMission, String reviewURL) {
+        this.progressMission = progressMission;
+        this.reviewURL = reviewURL;
+        this.reviewState = ReviewState.REQUEST;
+    }
+
+    public void complete() {
+        this.reviewState = ReviewState.COMPLETE;
+    }
+}
