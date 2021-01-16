958f2d7c62bbe828bff915926a43724e0d737b47
Nathan <skeks463@gmail.com>
2021-01-16 09:02:23 +0900

    feat : add restTemplate config

---
+++ bomplatform-github/src/main/java/com/javabom/bomplatform/config/RestTemplateConfig.java
@@ -0,0 +1,21 @@
+package com.javabom.bomplatform.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplateBuilder builder = new RestTemplateBuilder();
+
+        return builder.setReadTimeout(Duration.ofMillis(3000))
+                .setConnectTimeout(Duration.ofMillis(3000))
+                .build();
+    }
+}
