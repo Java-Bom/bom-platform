ef68c7e1510e2a0cc8d221af492b123507257158
BangKiHyun <rlrlvh@naver.com>
2021-01-21 17:09:43 +0900

    feat: add core config

---
+++ bomplatform-core/src/main/java/com/javabom/bomplatform/core/config/CoreConfig.java
@@ -0,0 +1,9 @@
+package com.javabom.bomplatform.core.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.javabom.bomplatform.core")
+public class CoreConfig {
+}
