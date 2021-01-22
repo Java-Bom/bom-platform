0e44357b160f6d393c1b5fead77dd3571a5ad412
Gobukgol <zxczc159@gmail.com>
2021-01-22 16:41:53 +0900

    feat(QueryDslConfiguration) : QueryDslConfiguration 추가

--- bomplatform-web/src/main/java/com/javabom/bomplatform/web/config/WebConfig.java
+++ bomplatform-web/src/main/java/com/javabom/bomplatform/web/config/WebConfig.java
@@ -1,12 +1,15 @@
 package com.javabom.bomplatform.web.config;
 
+import com.javabom.bomplatform.core.config.QueryDslConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * @Import 어노테이션으로 다른 모듈의 Configuration 클래스를 가져와서 다른 모듈의 컴포넌트 스캔을 진행한다.
  */
 @Configuration
 @ComponentScan(basePackages = {"com.javabom.bomplatform.web"})
+@Import({QueryDslConfiguration.class})
 public class WebConfig {
 }
