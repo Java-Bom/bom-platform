979f69c4c805ae9551027cf1c31ffc255806d26e
Gobukgol <zxczc159@gmail.com>
2021-02-27 09:39:17 +0900

    refactor(Controller 메소드) : 리뷰 완료 Controller GetMapping 에서 PutMapping으로 변경

--- bomplatform-web/src/main/java/com/javabom/bomplatform/web/review/controller/ReviewController.java
+++ bomplatform-web/src/main/java/com/javabom/bomplatform/web/review/controller/ReviewController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -48,7 +49,7 @@ public class ReviewController {
         return ResponseEntity.ok().body(reviewDtos);
     }
 
-    @GetMapping("/{reviewId}/complete")
+    @PutMapping("/{reviewId}/complete")
     public ResponseEntity<Void> completeReview(@PathVariable long reviewId, @RequestBody String challengerId) {
         reviewService.completeReviewState(reviewId, challengerId);
 
